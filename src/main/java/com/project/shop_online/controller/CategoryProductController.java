c23a471367239b8714a632b78982165f13d22aea
ThanhHiep1998 <hothanhhiepc5@gmail.com>
2020-11-24 10:56:46 +0700

    Controller
    Write Controller for all.

---
+++ src/main/java/com/project/shop_online/controller/CategoryProductController.java
@@ -0,0 +1,7 @@
+package com.project.shop_online.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryProductController {
+}
---
+++ src/main/java/com/project/shop_online/controller/MaterialController.java
@@ -0,0 +1,4 @@
+package com.project.shop_online.controller;
+
+public class MaterialController {
+}
