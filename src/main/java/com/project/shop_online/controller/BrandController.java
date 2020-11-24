d7cc931499f8f548f5bcd2ec1722832be8fada8d
ThanhHiep1998 <hothanhhiepc5@gmail.com>
2020-11-24 11:06:08 +0700

    a

--- src/main/java/com/project/shop_online/controller/BrandController.java
+++ src/main/java/com/project/shop_online/controller/BrandController.java
@@ -1,12 +1,9 @@
 package com.project.shop_online.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BrandController {
-    @RequestMapping("/a")
-    public String home() {
-        return "index";
-    }
+
 }
