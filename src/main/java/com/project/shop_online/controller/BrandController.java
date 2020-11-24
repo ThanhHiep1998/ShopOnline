64cbdbdb02cd244ce7ff7fe1c9cca547a18d5b1c
ThanhHiep1998 <hothanhhiepc5@gmail.com>
2020-11-24 11:05:10 +0700

    Update BrandController.java

--- src/main/java/com/project/shop_online/controller/BrandController.java
+++ src/main/java/com/project/shop_online/controller/BrandController.java
@@ -1,7 +1,12 @@
 package com.project.shop_online.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BrandController {
+    @RequestMapping("/a")
+    public String home() {
+        return "index";
+    }
 }
